430e1a32231ea3d18bf09936bcd5f57c6b670c24
Yago Castello <yagodealmeidacs@gmail.com>
2025-04-28 15:44:16 -0300

    Create Arrays_revisao.java

---
+++ Java10x/src/Iniciante/Condicoes/Arrays_revisao.java
@@ -0,0 +1,25 @@
+package Iniciante.Condicoes;
+
+public class Arrays_revisao {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[5];
+        ninja[0] = "Naruto  Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        ninja[3] = "Hinata Hyuga";
+
+        System.out.println(ninja[4]);
+
+
+        int [] idades = new int[2];
+        System.out.println(idades[1]);
+
+        boolean[] verdadeiro = new boolean[1];
+        System.out.println(verdadeiro[0]);
+
+
+        double[] flutuante = new double[1];
+        System.out.println(flutuante[0]);
+    }
+}
